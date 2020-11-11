f851fd5696a8293581b2410cfbd5d7009ba10343
Katie Strong <katiestrong123@gmail.com>
2020-11-11 11:40:45 +0000

    Drink class and test added

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -5,4 +5,8 @@ class TestDrink(unittest.TestCase):
     def setUp(self):
         self.drink = Drink("Lager", 4.5)
 
+    def test_drink_has_name(self):
+        self.assertEqual("Lager", self.drink.name)
 
+    def test_drink_has_price(self):
+        self.assertEqual(4.5, self.drink.price)
